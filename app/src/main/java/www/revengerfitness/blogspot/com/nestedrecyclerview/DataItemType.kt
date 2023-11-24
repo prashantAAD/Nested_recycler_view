97ee97fd38c0b439c7944982521ec54d59c34205
prashant <prashant191000@gmail.com>
2023-11-24 11:44:56 +0530

    Initial commit

---
+++ app/src/main/java/www/revengerfitness/blogspot/com/nestedrecyclerview/DataItemType.kt
@@ -0,0 +1,10 @@
+package www.revengerfitness.blogspot.com.nestedrecyclerview
+
+class DataItemType {
+
+    companion object{
+        const val BEST_SELLER = 0
+        const val CLOTHING = 1
+        const val BANNER = 2
+    }
+}
